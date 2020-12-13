df9385eaa550760d484994ad7a5849fec93df0f5
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2020-12-13 01:25:32 -0500

    gui::event_widget: fixes

--- gui/event_widget.cpp
+++ gui/event_widget.cpp
@@ -48,11 +48,10 @@ void event_widget::stop()
 ////////////////////////////////////////////////////////////////////////////////
 void event_widget::reset()
 {
-    stop();
-
     std::tm tm { 0 /*sec*/, 0 /*min*/, 0 /*hr*/, 1 /*day*/, 1 /*mon*/, 0 /*year*/ };
     epoch_ = src::system_clock::from_time_t(std::mktime(&tm));
 
+    stop();
     time(epoch_);
 }
 
@@ -63,9 +62,9 @@ void event_widget::click(where w, src::seconds s)
     {
         switch(w)
         {
-        case top: epoch_ += s; break;
-        case bottom: epoch_ -= s; break;
+        case top: start_ -= s; break;
         case middle: stop(); break;
+        case bottom: start_ += s; break;
         }
     }
     else if(w == middle) start();
