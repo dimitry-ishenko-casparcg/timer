df6c7b39146f86a73d2cd0c44560078ba987f5bd
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2020-12-16 17:22:12 -0500

    gui::video_widget: implement name and time processing

--- gui/video_widget.cpp
+++ gui/video_widget.cpp
@@ -6,15 +6,73 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 #include "video_widget.hpp"
+#include <QColor>
+#include <ctime>
+
+using namespace std::chrono_literals;
 
 ////////////////////////////////////////////////////////////////////////////////
 namespace gui
 {
 
 ////////////////////////////////////////////////////////////////////////////////
+const QColor dark_gray(42, 42, 42);
+
+////////////////////////////////////////////////////////////////////////////////
 video_widget::video_widget(QWidget* parent) : QWidget(parent)
 {
     ui_.setupUi(this);
+
+    std::tm tm { 0 /*sec*/, 0 /*min*/, 0 /*hr*/, 1 /*day*/, 1 /*mon*/, 0 /*year*/ };
+    epoch_ = src::system_clock::from_time_t(std::mktime(&tm));
+
+    reset();
+    timer_.setInterval(3s);
+    timer_.setSingleShot(true);
+    connect(&timer_, &QTimer::timeout, this, &video_widget::reset);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void video_widget::name(const QString& name)
+{
+    timer_.start();
+    ui_.name->setText(name);
+    color(Qt::white);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void video_widget::time(src::time_point time, src::seconds total)
+{
+    if(time != time_)
+    {
+        timer_.start();
+        time_ = time;
+
+        auto done = time.time_since_epoch();
+        if(done > total) done = total;
+
+        switch(mode_)
+        {
+        case count_up: ui_.widget->time(epoch_ + done); break;
+        case count_down: ui_.widget->time(epoch_ + (total - done)); break;
+        }
+        ui_.widget->font_color(Qt::green);
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void video_widget::color(const QColor& c)
+{
+    ui_.name->setStyleSheet(QString("color: %1;").arg(c.name()));
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void video_widget::reset()
+{
+    timer_.stop();
+
+    color(dark_gray);
+    ui_.widget->font_color(dark_gray);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
--- gui/video_widget.hpp
+++ gui/video_widget.hpp
@@ -9,12 +9,23 @@
 #define GUI_VIDEO_WIDGET_HPP
 
 ////////////////////////////////////////////////////////////////////////////////
+#include "src/time.hpp"
 #include "ui_video_widget.h"
+
+#include <QTimer>
 #include <QWidget>
 
 ////////////////////////////////////////////////////////////////////////////////
 namespace gui
 {
+
+////////////////////////////////////////////////////////////////////////////////
+enum count_mode
+{
+    count_up,
+    count_down
+};
+
 
 ////////////////////////////////////////////////////////////////////////////////
 class video_widget : public QWidget
@@ -24,8 +35,21 @@ class video_widget : public QWidget
 public:
     explicit video_widget(QWidget* parent = nullptr);
 
+    auto mode() const { return mode_; }
+    void mode(count_mode mode) { mode_ = mode; }
+
+    void name(const QString&);
+    void time(src::time_point, src::seconds);
+    void reset();
+
 private:
     Ui::video_widget ui_;
+    QTimer timer_;
+
+    src::time_point epoch_, time_;
+    count_mode mode_ = count_down;
+
+    void color(const QColor&);
 };
 
 ////////////////////////////////////////////////////////////////////////////////
