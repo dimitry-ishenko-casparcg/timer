7e3a7be9173abae3e373048e528b968468e37895
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2020-12-16 21:05:58 -0500

    main: add option processing bits

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,89 @@
 #include "gui/main_window.hpp"
 
 #include <QApplication>
+#include <QCommandLineParser>
 #include <QFontDatabase>
 
-#include <exception>
 #include <iostream>
+#include <stdexcept>
+
+////////////////////////////////////////////////////////////////////////////////
+void add_options(QCommandLineParser& parser)
+{
+    parser.addOptions({
+    {
+        "full-screen",
+        "Show application in full screen.\n"
+    },
+    {
+        "port",
+        "Set UDP port to listen on.\n",
+        "n", "7205"
+    },
+    {
+        "channel",
+        "Set one or more channels to be monitored. "
+        "Channels can be specified as several comma-separated values and/or ranges. "
+        "Negative numbers are removed from the set.\n"
+        "This option can be repeated multiple times.\n",
+        "...", "1"
+    },
+    {
+        "layer",
+        "Set one or more layers to be monitored. "
+        "Layers can be specified as several comma-separated values and/or ranges. "
+        "Negative numbers are removed from the set.\n"
+        "This option can be repeated multiple times.\n",
+        "...", "10"
+    },
+    {
+        "name-size",
+        "Set font size for the event and video names.\n",
+        "pt", "20"
+    },
+    {
+        "clock-size",
+        "Set font size for the clock widget.\n",
+        "pt", "160"
+    },
+    {
+        "event-size",
+        "Set font size for the event widget.\n",
+        "pt", "120"
+    },
+    {
+        "video-size",
+        "Set font size for the video widget.\n",
+        "pt", "160"
+    }});
+    parser.addHelpOption();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+auto to_int(const QCommandLineParser& parser, const QString& option)
+{
+    auto value = parser.value(option);
+    bool ok;
+    auto n = value.toInt(&ok);
+
+    if(!ok) throw std::invalid_argument(
+        ("Invalid '" + option + "' value: " + value).toStdString()
+    );
+    return n;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+auto to_double(const QCommandLineParser& parser, const QString& option)
+{
+    auto value = parser.value(option);
+    bool ok;
+    auto n = value.toDouble(&ok);
+
+    if(!ok) throw std::invalid_argument(
+        ("Invalid '" + option + "' value: " + value).toStdString()
+    );
+    return n;
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char* argv[])
@@ -22,13 +101,22 @@ int main(int argc, char* argv[])
         QApplication app(argc, argv);
         QFontDatabase::addApplicationFont(":/seven.otf");
 
-        int port = 7250;
-        gui::window_mode mode = gui::windowed;
+        QCommandLineParser parser;
+        add_options(parser);
+        parser.process(app);
 
-        // process options
+        auto port = to_int(parser, "port");
+        auto mode = parser.isSet("full-screen") ? gui::full_screen : gui::windowed;
 
         gui::main_window win(port, mode);
         win.show();
+
+        // channels & layers
+
+        win. name_size(to_double(parser,  "name-size"));
+        win.clock_size(to_double(parser, "clock-size"));
+        win.event_size(to_double(parser, "event-size"));
+        win.video_size(to_double(parser, "video-size"));
 
         exit_code = app.exec();
     }
